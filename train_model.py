4b9b40f704792e448e557394f0dc204ef16872eb
qamarsobhy7-source <qamarsobhy7@gmail.com>
2026-09-02 01:26:21 +0300

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+# بناء هيكل الموديل (CNN Architecture) للتعرف على أمراض النباتات
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
+    MaxPooling2D(2, 2),
+    
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(5, activation='softmax') # 5 فئات للأمراض
+])
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
